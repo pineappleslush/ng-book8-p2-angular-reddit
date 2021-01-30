44f7db1a0c4f19ee3ae03d5d53185b3ab0b1e791
lani <lanimorton356@gmail.com>
2021-01-30 15:23:55 -0800

    Rendering a List of Articles

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 import {Article} from './article.model';
 
 @Component({
@@ -9,14 +9,9 @@ import {Article} from './article.model';
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
 
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article(
-      'Angular',
-      'http://angular.io',
-      10
-    );
   }
 
   ngOnInit(): void {
